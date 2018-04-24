04281ecf120d6f9c476f51f5a726f5f92b3eaec2
Zeljko Kalezic <zeljkokalezic@gmail.com>
2018-04-24 20:00:28 +0200

    Add project files.

---
+++ WebShop2018/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebShop2018.Startup))]
+namespace WebShop2018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
