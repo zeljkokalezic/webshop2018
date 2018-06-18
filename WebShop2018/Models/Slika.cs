d055df062a4970af117d9ddb0fe10cc68bc71caa
Dusan Jovanovic <dusanjovanovic0806@gmail.com>
2018-06-18 17:37:36 +0200

    Domaci zadatak

---
+++ WebShop2018/Models/Slika.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShop2018.Models
+{
+    public class Slika
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+
+        public virtual Proizvod proizvod { get; set; }
+    }
+}
